02252692d9aea09c223d7ffb735924198895e886
goh0622 <122134041+goh0622@users.noreply.github.com>
2023-09-18 21:40:06 +0800

    [MBHS-93] Database schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,42 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 0) do
+ActiveRecord::Schema[7.0].define(version: 2023_09_18_131102) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+  create_table "applications", force: :cascade do |t|
+    t.bigint "job_id", null: false
+    t.bigint "candidate_id", null: false
+    t.datetime "submitted_at"
+    t.string "status"
+    t.string "notes"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["job_id"], name: "index_applications_on_job_id"
+  end
+
+  create_table "job_types", force: :cascade do |t|
+    t.string "name", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "jobs", force: :cascade do |t|
+    t.bigint "job_type_id", null: false
+    t.bigint "company_id", null: false
+    t.string "title"
+    t.text "description"
+    t.string "requirements"
+    t.string "expired_date"
+    t.string "status"
+    t.string "location"
+    t.integer "no_of_vacancies"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["job_type_id"], name: "index_jobs_on_job_type_id"
+  end
+
+  add_foreign_key "applications", "jobs"
+  add_foreign_key "jobs", "job_types"
 end
